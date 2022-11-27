ec11ed950d3ec33c4cf720d20f96c97698647787
Riki aldi pari <rikialdipari@gmail.com>
2022-11-27 16:21:52 +0700

    ad all

--- Karyawan.java
+++ Karyawan.java
@@ -1,6 +1,6 @@
 public class Karyawan {
     public static void main(String[] args) {
         System.out.println("Hello Word");
-         System.out.println("Hello Word update dari github langsung");
+        System.out.println("Hello Word update dari github langsung");
     }
 }
